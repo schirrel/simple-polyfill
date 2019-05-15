10ff733f63f36bed599871d0dc37268ad6c1977e
Alan Schio <CoderSquirrel@users.noreply.github.com>
2019-05-15 17:51:48 -0400

    Update finders.js

--- finders.js
+++ finders.js
@@ -4,3 +4,5 @@
  
  // iterare all objects find images in bg
 $('*').toArray().map((item)=> { return item.style.backgroundImage ||item.style.background.indexOf('url') != -1; }).filter((img)=> {return img}).forEach((img)=>{ console.log(img)})
+
+Array.from($('*')).map((item)=> { return item.style.backgroundImage ||item.style.background.indexOf('url') != -1; }).filter((img)=> {return img}).forEach((img)=>{ console.log(img)})
